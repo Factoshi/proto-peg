65739863eec1e861ba9815eaf406bea6b558c967
Alex Carrithers <38507729+afenrir@users.noreply.github.com>
2020-08-22 15:12:42 +0100

    refactor: move .proto schema to top level

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,7 @@ export default class ProtoPeg {
      * Initialises the ProtoPeg instance. Must be called once before encoding or decoding price records.
      */
     public async init() {
-        this.root = await protobuf.load(path.join(__dirname, 'priceRecord.proto'));
+        this.root = await protobuf.load(path.join(__dirname, '../priceRecord.proto'));
     }
 
     /**
